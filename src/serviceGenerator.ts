767d32641b51a31235aeb0a26eb5fa2102129f35
nopsky <nopsky@nopskys-MacBook-Air.local>
2023-08-20 14:40:37 +0800

    feat: 如果format类型是64或者long,则用string代替

--- src/serviceGenerator.ts
+++ src/serviceGenerator.ts
@@ -112,7 +112,11 @@ const getType = (schemaObject: SchemaObject | undefined, namespace: string = '')
   const stringEnum = ['string', 'email', 'password', 'url', 'byte', 'binary'];
 
   if (numberEnum.includes(schemaObject.format)) {
-    type = 'number';
+    if(schemaObject.format === 'int64' || schemaObject.format === 'long') {
+      type = 'string'
+    } else {
+      type = 'number';
+    }
   }
 
   if (schemaObject.enum) {
